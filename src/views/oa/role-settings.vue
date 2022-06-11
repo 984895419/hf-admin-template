84ba16b729fbb0c88bed0ba28b5aa845ae52f038
jiannansong <984895419@qq.com>
2022-06-11 17:01:24 +0800

    upd

--- src/views/oa/role-settings.vue
+++ src/views/oa/role-settings.vue
@@ -2,23 +2,20 @@
   <div class="app-container">
     <div class="container-main">
       <div>用户拥有的角色如下: </div>
-      <el-checkbox v-model="checkAll" :indeterminate="isIndeterminate" @change="handleCheckAllChange">全选</el-checkbox>
       <div style="margin: 15px 0;" />
       <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
-        <el-checkbox v-for="(item, index) in cities" :key="index" :label="item">{{ item.roleName }}
-        </el-checkbox>
+        <el-checkbox v-for="(item,index) in cities" :key="index" :label="item.roleName">{{ item.roleName }}</el-checkbox>
       </el-checkbox-group>
       <div class="main-btn">
         <el-button type="primary" @click="saveRole()">保存</el-button>
         <el-button @click="closeDialog()">取消</el-button>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-import { baseApiGetMethod } from '@/components/CURD/baseApi'
+import { baseApiGetMethod, baseApiPostMethod } from '@/components/CURD/baseApi'
 export default {
   props: {
     data: {
@@ -27,38 +24,22 @@ export default {
   },
   data() {
     return {
-      checkAll: false,
       checkedCities: [],
       cities: [],
       isIndeterminate: true,
       deepCloneData: []
     }
   },
-  mounted() {
-    this.init()
-  },
+
   created() {
     this.deepCloneData = this.data
+    this.viewRoleManagement()
+    this.getUserIdBindRole()
   },
   methods: {
-    handleCheckAllChange(val) {
-      this.checkedCities = val ? cityOptions : []
-      this.isIndeterminate = false
-    },
+
     handleCheckedCitiesChange(value) {
-      const checkedCount = value.length
-      this.checkAll = checkedCount === this.cities.length
-      this.isIndeterminate = checkedCount > 0 && checkedCount < this.cities.length
-    },
-    init() {
-      this.viewRoleManagement()
-      baseApiGetMethod(`/api/hfBaseUserInfo/bindRoles/${this.data.userId}`).then(
-        (resp) => {
-          if (resp.retCode === '00001') {
-            console.log(resp.data, 111)
-          }
-        }
-      )
+
     },
     // 查看角色管理
     viewRoleManagement() {
@@ -72,16 +53,40 @@ export default {
     },
     // 保存
     saveRole() {
-      let param = {}
-      param = {
-        // 'roleId': this.checkedCities.roleId,
-        // 'userIds': this.checkedCities.userIds
+      const roleIdList = []
+      if (this.checkedCities.length === 0) {
+        this.$message({
+          message: '还没有选择角色',
+          type: 'warning'
+        })
+      } else {
+        this.checkedCities.forEach((item, index) => {
+          roleIdList.push(item.roleId)
+        })
+        const param = {
+          'userId': this.data.userId,
+          'roleIds': roleIdList
+        }
+        baseApiPostMethod('/api/hfBaseUserInfo/bindRoles', param).then(
+          (resp) => {
+            if (resp.retCode === '00001') {
+              this.$message({
+                message: '操作成功',
+                type: 'success'
+              })
+              this.$emit('closeDialog')
+            }
+          }
+        )
       }
-      console.log(param)
-      baseApiGetMethod('/api/hfBaseRightRole/bindUsers', param).then(
+    },
+    // 获取用户绑定的角色
+    getUserIdBindRole() {
+      baseApiGetMethod(`/api/hfBaseUserInfo/bindRoles/${this.data.userId}`).then(
         (resp) => {
           if (resp.retCode === '00001') {
-             console.log(resp.data, 'bindUsers')
+            // this.$set(this.checkedCities, 0, resp.data)
+            this.checkedCities = resp.data
           }
         }
       )
