6f903aff81f52672b9feb4176ddc680bea8bee29
jiannansong <984895419@qq.com>
2022-08-01 08:24:24 +0800

    upd

---
+++ src/views/basic/basePeripheral/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="peripheral">
+    <iframe id="basic-peripheral" :src="urlredirect" scrolling="no" frameborder="0" />
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['urlredirect'],
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.peripheral {
+    width: 100%;
+    height: calc(100vh - 87px);
+    overflow-y: scroll;
+    overflow-x: hidden;
+
+    iframe {
+        width: 100%;
+        height: 100%;
+
+        body,
+        html,
+        #app {
+            overflow-y: scroll !important;
+            ;
+        }
+    }
+}
+</style>
